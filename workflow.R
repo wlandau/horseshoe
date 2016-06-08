f63fb93c4d6f1752f033cf51ba9e1319e5359c46
wlandau <will.landau@gmail.com>
2016-06-08 08:33:39 -0400

    Make sources = "R" in workflow.R

--- workflow.R
+++ workflow.R
@@ -1,9 +1,9 @@
 library(workflowHelper)
 
-sources = "R/paschold.R"
+sources = "R"
 
 datasets = commands(
   paschold_DE_1000 = paschold_DE(genes = 1e3)
 )
 
-plan_workflow(sources, datasets)
+plan_workflow(sources = sources, datasets = datasets)
