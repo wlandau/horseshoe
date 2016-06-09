a0a94abc1a91fbb2e460f8cee4e117360bc33287
wlandau <will.landau@gmail.com>
2016-06-09 09:18:39 -0500

    Add comments to workflow.R

--- workflow.R
+++ workflow.R
@@ -4,14 +4,14 @@ sources = c("config.R", paste0("R/", list.files("R")))
 packages = strings(fbseq, plyr)
 
 datasets = commands(
-  pascholdDE = paschold_DE(),
-  poissonDE = poisson_DE()
+  pascholdDE = paschold_DE(), # Paschold (2012) parents and 1000 genes
+  poissonDE = poisson_DE()    # Poisson counts in 2 groups with no overdispersion 
 )
 
 analyses = commands(
-  fb = fb(..DATASET..),
-  fbh = fbh(..DATASET..),
-  fbhh = fbhh(..DATASET..)
+  fb = fb(..DATASET..),    # fully Bayes, normal betas
+  fbh = fbh(..DATASET..),  # fully Bayes, horseshoe betas except beta_1
+  fbhh = fbhh(..DATASET..) # fully Bayes, all horseshoe
 )
 
 summaries = commands(
